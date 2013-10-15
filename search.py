0f304ec929cdeb9508f825c58798721d29f14f89
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2013-10-14 22:14:49 -0500

    Added an equation database, gitignore, and code to skip comment lines.

--- search.py
+++ search.py
@@ -2,6 +2,8 @@
 query = raw_input().lower() # can take a commandline argument or a piped text stream
 with open('equation-database.txt','r') as f:
     for line in f:
+        if '%%' in line:
+             pass
         if query in line.lower():
              print line
              while '%%' not in line:
