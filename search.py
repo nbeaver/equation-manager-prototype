1136538aa60ee524e263b856f49a9dbfc2414d13
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2013-10-14 23:28:12 -0500

    Changed file I/O so that it reads everything into memory and splits it up. If the text file gets too big I'll worry about it later.

--- search.py
+++ search.py
@@ -7,14 +7,13 @@ if query == '': # if there's still nothing, then print a message and exit.
     sys.exit("Usage: search <query here>")
     
 with open('equation-database.txt','r') as f:
-    for line in f:
-        if '%%' in line:
-             pass # Go to the next line
-        if query in line.lower():
-             print line,
-             while '%%' not in line:
-                 try:
-                     line = f.next()
-                     print line,
-                 except StopIteration:
-                     break # If we hit the end of the file, exit the loop.
+    for record in f.read().split('%%'):
+        if record == "":
+            pass
+        lines =  record.split('\n')
+        try:
+            first_line = lines[1].lower()
+            if query in first_line:
+                print record.strip()
+        except IndexError:
+            pass
