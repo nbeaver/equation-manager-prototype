3cd40493b90ce5d03d0e2b067794d07511d33cb2
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2013-10-14 21:39:05 -0500

    Very simple file-reading loop; haven't even tested it yet.

---
+++ search.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+query = raw_input().lower() # can take a commandline argument or a piped text stream
+with open('equation-database.txt','r') as f:
+    for line in f:
+        if query in line.lower():
+             print line
+             while '%%' not in line:
+                 print next(f)
