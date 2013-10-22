d4897a1a4371600f6f133cbae1ad3afe12f8bfb4
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2013-10-22 16:09:02 -0500

    Moved the matching function out of the main for loop and added more comments for clarity.

--- search.py
+++ search.py
@@ -3,24 +3,34 @@ import os, sys
 directory = os.path.dirname(__file__)
 database = os.path.join(directory, './equation-database.txt')
 
-query = ''.join(sys.argv[1:]) # check commandline arguments first
-if query == '-': # if the only argument is '-', check stdin.
+query = ''.join(sys.argv[1:]) # Check commandline arguments first.
+if query == '-': # If the only argument is '-', check stdin.
     query = sys.stdin.readlines()
-if query == '': # if there's still nothing, then print a message and exit.
+if query == '': # If there's still nothing, then print a message and exit.
     sys.exit("Usage: search <query here>")
 
-query_lowercase = query.lower() # we'll make the comparison in lowercase, too, so it's case insensitive
+query_lowercase = query.lower() # We'll make the comparison in lowercase, too, so it's case-insensitive.
     
 f = open(database,'r')
+
+def match(query, candidate):
+    # For the moment, just checking if the query is in the candidate string is sufficient.
+    if query in candidate:
+        return True
+    else:
+        return False
+
 for record in f.read().split('%%'):
     if record == "":
         pass
     lines = record.split('\n')
     try:
         first_line_lowercase = lines[1].lower()
-        if query_lowercase in first_line_lowercase:
-            print record.strip()
+        # For the moment, we just check the first string of the record for matches to the query.
+        if match(query_lowercase, first_line_lowercase):
+            print record.strip() # Remove the linefeeds
+            # Add a visual separator between multiple matches.
             print '--------------------------------------------------------------------------------'
-    except IndexError:
+    except IndexError: # This happens if, e.g. there's an empty record so len(lines)==0.
         pass
 f.close()
